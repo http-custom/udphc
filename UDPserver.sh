464e3e7a26287bf1b2d2d750b502aeee5be82e65
http-custom <137656009+http-custom@users.noreply.github.com>
2024-02-26 16:22:53 -0300

    Add files via upload

--- UDPserver.sh
+++ UDPserver.sh
@@ -627,15 +627,6 @@ download_udpServer(){
 	fi
 }
 
-make_service(){
-	ip_nat=$(ip -4 addr | grep inet | grep -vE '127(\.[0-9]{1,3}){3}' | cut -d '/' -f 1 | grep -oE '[0-9]{1,3}(\.[0-9]{1,3}){3}' | sed -n 1p)
-	interfas=$(ip -4 addr | grep inet | grep -vE '127(\.[0-9]{1,3}){3}'|grep "$ip_nat"|awk {'print $NF'})
-	ip_publica=$(grep -m 1 -oE '^[0-9]{1,3}(\.[0-9]{1,3}){3}$' <<< "$(wget -T 10 -t 1 -4qO- "http://ip1.dynupdate.no-ip.com/" || curl -m 10 -4Ls "http://ip1.dynupdate.no-ip.com/")")
-
-	#ip_nat=$(fun_ip nat)
-	#interfas=$(ip -4 addr | grep inet | grep -vE '127(\.[0-9]{1,3}){3}'|grep "$ip_nat"|awk {'print $NF'})
-	#ip_publica=$(fun_ip)
-
 cat <<EOF > /etc/systemd/system/UDPserver.service
 [Unit]
 Description=UDPserver Service by @Rufu99
