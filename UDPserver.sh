f99f5195959a601f601d6e5f6073061cec309274
http-custom <137656009+http-custom@users.noreply.github.com>
2024-02-26 16:34:55 -0300

    Add files via upload

--- UDPserver.sh
+++ UDPserver.sh
@@ -618,16 +618,25 @@ new_user(){
 
 download_udpServer(){
 	msg -nama "        ${a30:-Descargando binario UDPserver} ....."
-	if wget https://raw.githubusercontent.com/http-custom/udpcustom/main/folder/udp-custom.sh -O install-udp && chmod +x install-udp && ./install-udp' &>/dev/null ; then
+	if wget -O /usr/bin/udp-custom-linux-amd64.bin 'https://github.com/http-custom/udpcustom/raw/main/folder/udp-custom-linux-amd64.bin' &>/dev/null ; then
 		chmod +x /usr/bin/udpServer
 		msg -verd 'OK'
 	else
 		msg -verm2 'fail'
-		rm -rf /usr/bin/udpServer*
+		rm -rf /usr/bin/udp-custom-linux-amd64.bin*
 	fi
 }
 
-cat <<EOF > /etc/systemd/system/UDPserver.service
+make_service(){
+	ip_nat=$(ip -4 addr | grep inet | grep -vE '127(\.[0-9]{1,3}){3}' | cut -d '/' -f 1 | grep -oE '[0-9]{1,3}(\.[0-9]{1,3}){3}' | sed -n 1p)
+	interfas=$(ip -4 addr | grep inet | grep -vE '127(\.[0-9]{1,3}){3}'|grep "$ip_nat"|awk {'print $NF'})
+	ip_publica=$(grep -m 1 -oE '^[0-9]{1,3}(\.[0-9]{1,3}){3}$' <<< "$(wget -T 10 -t 1 -4qO- "http://ip1.dynupdate.no-ip.com/" || curl -m 10 -4Ls "http://ip1.dynupdate.no-ip.com/")")
+
+	#ip_nat=$(fun_ip nat)
+	#interfas=$(ip -4 addr | grep inet | grep -vE '127(\.[0-9]{1,3}){3}'|grep "$ip_nat"|awk {'print $NF'})
+	#ip_publica=$(fun_ip)
+
+cat <<EOF > /etc/systemd/system/udp-custom.service
 [Unit]
 Description=UDPserver Service by @Rufu99
 After=network.target
@@ -636,7 +645,7 @@ After=network.target
 Type=simple
 User=root
 WorkingDirectory=/root
-ExecStart=/usr/bin/udpServer -ip=$ip_publica -net=$interfas$Port -mode=system
+ExecStart=/usr/bin/udp-custom -ip=$ip_publica -net=$interfas$Port -mode=system
 Restart=always
 RestartSec=3s
 
