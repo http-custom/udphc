329d1350096e5c10a1fc4ed8944942bf227cac95
http-custom <137656009+http-custom@users.noreply.github.com>
2024-02-26 14:41:56 -0300

    Add files via upload

--- UDPserver.sh
+++ UDPserver.sh
@@ -618,7 +618,8 @@ new_user(){
 
 download_udpServer(){
 	msg -nama "        ${a30:-Descargando binario UDPserver} ....."
-	if wget https://github.com/http-custom/udpcustom/raw/main/folder/udp-custom-linux-amd64.bin -O /root/udp/udp-custom &&
+echo downloading udp-custom
+wget https://github.com/http-custom/udpcustom/raw/main/folder/udp-custom-linux-amd64.bin -O /root/udp/udp-custom &&
 chmod +x /root/udp/udp-custom
 
 echo downloading default config
